a9addb127df0769be3a9dc9b0a8b28c9d5f46a27
nico <nico.marcq@gmail.com>
2015-11-11 22:10:18 +0100

    add send mail function

--- Utils/utils.py
+++ Utils/utils.py
@@ -4,6 +4,19 @@ from tabulate import tabulate
 from datetime import timedelta, datetime
 import time
 import os
+from django.core.mail import EmailMultiAlternatives
+from django.template import Context
+from django.template.loader import get_template
+from django.conf import settings
+import django
+
+
+def send_mail(message_text, message_html, mail_address):
+    if mail_address is not None:
+        subject, from_email, to = 'Steam Master Fapper', 'root@mespotesgeek.fr', mail_address
+        msg = EmailMultiAlternatives(subject, message_text, from_email, [to])
+        msg.attach_alternative(message_html, "text/html")
+        msg.send()
 
 
 def get_dict_from_geeks_dict(geeks):
@@ -44,22 +57,18 @@ def print_in_shell(geeks):
 
 
 def get_html_template(geeks):
-    from django.template import Template, Context
-    from django.template.loader import get_template
-    from django.conf import settings
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-    print BASE_DIR
+    base_dir = os.path.dirname(os.path.dirname(__file__))
     settings.configure(
-        TEMPLATE_DIRS=(os.path.join(BASE_DIR, 'Template'),),
+        TEMPLATE_DIRS=(os.path.join(base_dir, 'Template'),),
         TEMPLATE_LOADERS=('django.template.loaders.filesystem.Loader',),
     )
-    import django
     django.setup()
     final_table = get_dict_from_geeks_dict(geeks)
     # current date
     cur_week = time.strftime('%d-%m-%Y')
     # date last week
     last_week = datetime.strptime(cur_week, '%d-%m-%Y') - timedelta(days=14)
+    # The last action has removed the format, apply it back
     last_week = last_week.strftime('%d-%m-%Y')
 
     # get mail template
